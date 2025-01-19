9d46310b5fa83762591953ee7ac6b2a96d3f2b2f
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-01-19 23:41:19 +0800

    added thumbnail saving

--- app/Services/Media/MediaVideosService.php
+++ app/Services/Media/MediaVideosService.php
@@ -8,6 +8,7 @@ use App\Models\Media\MediaVideo;
 use App\Repositories\MediaVideosRepository;
 
 const VIDEO_UPLOADS_DIR = '/uploads/videos/';
+const VIDEO_THUMBNAILS_DIR = '/uploads/videos/';
 const THUMBNAIL_PLACEHOLDER = 'https://placehold.co/600x400?text=no+video+available';
 
 class MediaVideosService extends MediaVideosRepository
@@ -22,6 +23,11 @@ class MediaVideosService extends MediaVideosRepository
         return config('app.url').'files?path='.VIDEO_UPLOADS_DIR.$filename;
     }
 
+    private function createThumbnailUrl($filename)
+    {
+        return config('app.url').'files?path='.VIDEO_THUMBNAILS_DIR.$filename;
+    }
+
     public function create($data)
     {
         $data['name_slug'] = Str::slug($data['name']);
@@ -29,14 +35,21 @@ class MediaVideosService extends MediaVideosRepository
         if ($data['video_file']) {
 
             $video = $data['video_file'];
+            $videoFilename = Str::slug($video->getClientOriginalName().time()).'.'.$video->getClientOriginalExtension();
+            $videoFilepath = VIDEO_UPLOADS_DIR.$videoFilename;
+            Storage::disk('public')->put($videoFilepath, file_get_contents($video));
 
-            $filename = Str::slug($video->getClientOriginalName().time()).'.'.$video->getClientOriginalExtension();
-            $filepath = VIDEO_UPLOADS_DIR.$filename;
+            if ($data['thumbnail_file'])
+            {
+                $thumbnail = $data['thumbnail_file'];
+                $thumbnailFilename = Str::slug($thumbnail->getClientOriginalName().time()).'.'.$thumbnail->getClientOriginalExtension();
+                $thumbnailFilepath = VIDEO_UPLOADS_DIR.$thumbnailFilename;
 
-            Storage::disk('public')->put($filepath, file_get_contents($video));
+                Storage::disk('public')->put($thumbnailFilepath, file_get_contents($thumbnail));
+            }
 
-            $data['video_url'] = $this->createVideoURL($filename);
-            $data['thumbnail_url'] = $data['thumbnail_file'] ? Storage::url('thumbnails/' . $data['name_slug'] . '.png') : THUMBNAIL_PLACEHOLDER;
+            $data['video_url'] = $this->createVideoURL($videoFilename);
+            $data['thumbnail_url'] = $data['thumbnail_file'] ? $this->createVideoURL($thumbnailFilename) : THUMBNAIL_PLACEHOLDER;
 
             unset($data['video_file']);
             unset($data['thumbnail_file']);
